e6050700a3f8223ec720fb3d227ca79588183083
Joshua Malomo <rexunborn@gmail.com>
2020-12-04 17:23:02 +0100

    Added #moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -7,10 +7,20 @@ class Knight
   def initialize(start)
     # start is starting position as array
     @start = start
-    @possible_moves = nil
+    @possible_moves = moves(start)
+  end
+
+  def moves(arr)
+    new_arr = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2]]
+    possible = new_arr.map do |num1, num2|
+      [arr[0] + num1, arr[1] + num2]
+    end
+    possible.reject { |x, y| invalid?(x, y) }
   end
 
   def invalid?(var1, var2)
     var1.negative? || var1 > 7 || var2.negative? || var2 > 7
   end
 end
+
+# p Knight.new([0, 0]).moves([0, 0])
