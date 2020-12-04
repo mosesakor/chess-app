3f7f0fccb8c5e8437188a8e3cec7ffd41fdc96dc
Joshua Malomo <rexunborn@gmail.com>
2020-12-04 17:20:27 +0100

    Added #invalid-check

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -5,7 +5,12 @@ class Knight
   attr_reader :start, :possible_moves
 
   def initialize(start)
+    # start is starting position as array
     @start = start
-    @possible_moves = moves(start)
+    @possible_moves = nil
+  end
+
+  def invalid?(var1, var2)
+    var1.negative? || var1 > 7 || var2.negative? || var2 > 7
   end
 end
